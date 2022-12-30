98a31c7e953e8934b3417a33183339dad8669a31
OfirBittan <109820744+OfirBittan@users.noreply.github.com>
2022-12-30 18:03:57 +0200

    check commit.

--- Main Project.py
+++ Main Project.py
@@ -9,41 +9,41 @@ from sklearn.ensemble import RandomForestClassifier
 from sklearn.naive_bayes import GaussianNB
 
 if __name__ == "__main__":
-    # print("-- Start --")
-    # num_of_hotel_pages = int(input("How many pages do you want to crawl? "))
-    #
-    # tripadvisor_url_short = "https://www.tripadvisor.com"
-    # tripadvisor_urls = ["https://www.tripadvisor.com/Hotels-g60763-New_York_City_New_York-Hotels.html",
-    #                     "https://www.tripadvisor.com/Hotels-g187147-Paris_Ile_de_France-Hotels.html",
-    #                     "https://www.tripadvisor.com/Hotels-g274707-Prague_Bohemia-Hotels.html",
-    #                     "https://www.tripadvisor.com/Hotels-g186338-London_England-Hotels.html",
-    #                     "https://www.tripadvisor.com/Hotels-g274887-Budapest_Central_Hungary-Hotels.html",
-    #                     "https://www.tripadvisor.com/Hotels-g28930-Florida-Hotels.html"]
-    #
-    # print("-- Crawling --")
-    # for url in tripadvisor_urls:
-    #     soup = create_soup_obj(url)
-    #     all_hotels_parse(num_of_hotel_pages, soup, tripadvisor_url_short)
-    # specific_hotel_parse()
-    # df = create_df()
-    # df.to_csv(r'data frame before cleaning.csv', index=False, header=True)
-    #
-    # print("-- Cleaning --")
-    # remove_duplicative(df)
-    # remove_corrupt_rows(df)
-    # df = outlier_detection_zscore_dist(df)
-    # df.to_csv(r'data frame after cleaning.csv', index=False, header=True)
-    #
-    # print("-- Visualization --")
-    # sr = get_frequent_elements(df, "Tripadvisor rating")
-    # fig, axes = plt.subplots(1, 2, figsize=(20, 5))
-    # one_dim_plot(sr, "pie", axes[0])
-    # one_dim_plot(sr, "bar", axes[1])
-    # plt.savefig("OneDimPlot.png")
-    # plt.close()
-    # two_dim_plot(df["Tripadvisor rating"], df["Cleanliness rating"])
-    # plt.savefig("TwoDimPlot.png")
-    # plt.close()
+    print("-- Start --")
+    num_of_hotel_pages = int(input("How many pages do you want to crawl? "))
+
+    tripadvisor_url_short = "https://www.tripadvisor.com"
+    tripadvisor_urls = ["https://www.tripadvisor.com/Hotels-g60763-New_York_City_New_York-Hotels.html",
+                        "https://www.tripadvisor.com/Hotels-g187147-Paris_Ile_de_France-Hotels.html",
+                        "https://www.tripadvisor.com/Hotels-g274707-Prague_Bohemia-Hotels.html",
+                        "https://www.tripadvisor.com/Hotels-g186338-London_England-Hotels.html",
+                        "https://www.tripadvisor.com/Hotels-g274887-Budapest_Central_Hungary-Hotels.html",
+                        "https://www.tripadvisor.com/Hotels-g28930-Florida-Hotels.html"]
+
+    print("-- Crawling --")
+    for url in tripadvisor_urls:
+        soup = create_soup_obj(url)
+        all_hotels_parse(num_of_hotel_pages, soup, tripadvisor_url_short)
+    specific_hotel_parse()
+    df = create_df()
+    df.to_csv(r'data frame before cleaning.csv', index=False, header=True)
+
+    print("-- Cleaning --")
+    remove_duplicative(df)
+    remove_corrupt_rows(df)
+    df = outlier_detection_zscore_dist(df)
+    df.to_csv(r'data frame after cleaning.csv', index=False, header=True)
+
+    print("-- Visualization --")
+    sr = get_frequent_elements(df, "Tripadvisor rating")
+    fig, axes = plt.subplots(1, 2, figsize=(20, 5))
+    one_dim_plot(sr, "pie", axes[0])
+    one_dim_plot(sr, "bar", axes[1])
+    plt.savefig("OneDimPlot.png")
+    plt.close()
+    two_dim_plot(df["Tripadvisor rating"], df["Cleanliness rating"])
+    plt.savefig("TwoDimPlot.png")
+    plt.close()
 
     print("-- Machine Learning --")
     file_name = "data frame after cleaning.csv"
