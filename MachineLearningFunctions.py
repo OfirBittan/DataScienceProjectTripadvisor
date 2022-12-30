70da9f0ef79242df93d8fcce9faecde4700ba3f3
OfirBittan <109820744+OfirBittan@users.noreply.github.com>
2022-12-30 17:42:28 +0200

    30.12.2022 - before final fixes

--- MachineLearningFunctions.py
+++ MachineLearningFunctions.py
@@ -1,28 +1,5 @@
-import os  # for testing use only
-
-import numpy as np
 import pandas as pd
 from sklearn.model_selection import train_test_split
-# --------cross-validation
-from sklearn.model_selection import cross_val_score
-from sklearn.model_selection import GridSearchCV
-# -------- classification
-import sklearn
-from sklearn import neighbors, tree, ensemble, naive_bayes, svm
-# *** KNN
-from sklearn.neighbors import KNeighborsClassifier
-# *** Decision Tree; Random Forest
-from sklearn import tree
-from sklearn.tree import DecisionTreeClassifier
-from sklearn.ensemble import RandomForestClassifier
-# *** Naive Bayes
-from sklearn.naive_bayes import GaussianNB
-# *** SVM classifier
-from sklearn.svm import SVC
-# --------  metrics:
-from sklearn import metrics
-from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score, confusion_matrix
-from sklearn.metrics import make_scorer
 
 
 def load_dataset(file_name):
@@ -30,47 +7,23 @@ def load_dataset(file_name):
 
 
 def transfer_str_to_numeric_vals(dataset):
-    dt_copy = dataset.copy()
-    dt_copy = dataset.dropna(axis='index', how='any')
-    dt_copy = dt_copy.drop_duplicates(keep='first')
-    for i in dt_copy.columns:
-        i_label = dt_copy[i].astype('category').cat.categories.tolist()
-        instead = {i: {p: t for p, t in zip(i_label, list(range(0, len(i_label) + 1)))}}
-        dt_copy = dt_copy.replace(instead)
-    return dt_copy
+    dt = dataset.copy()
+    dt = dt.dropna(axis='index', how='any')
+    dt.drop_duplicates(keep='first', inplace=True)
+    for col in dt.columns:
+        lables = dt[col].astype('category').cat.categories.tolist()
+        replace__map_comp = {col: {k: v for k, v in zip(lables, list(range(0, len(lables) + 1)))}}
+        dt.replace(replace__map_comp, inplace=True)
+    return dt
 
 
 def split_to_train_and_test(dataset, label_column, test_ratio, rand_state):
-    j = 0
-    X = dataset.copy()
-    y = dataset.copy()
-    for data in X.columns:
-        if data == label_column:
-            y = X.iloc[:, j]
-            X.drop(X.columns[j], axis=1, inplace=True)
-        else:
-            j += 1
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=test_ratio, random_state=rand_state)
+    i = 0
+    for col in dataset.columns:
+        if col == label_column:
+            y = dataset.iloc[:, i]
+            dataset.drop(dataset.columns[i], axis=1, inplace=True)
+            continue
+        i += 1
+    X_train, X_test, y_train, y_test = train_test_split(dataset, y, test_size=test_ratio, random_state=rand_state)
     return X_train, X_test, y_train, y_test
-
-
-def find_best_model(X_train, y_train, max_depth_val, min_samples_split_val):
-    recall_score = make_scorer(metrics.recall_score)
-    print(recall_score)
-    clf1 = DecisionTreeClassifier(max_depth=max_depth_val, min_samples_split=min_samples_split_val)
-    clf1_cross = cross_val_score(clf1, X_train, y_train, scoring=recall_score, cv=5)
-    clf1_score = clf1_cross.mean()
-    clf2 = GaussianNB()
-    clf2_cross = cross_val_score(clf2, X_train, y_train, scoring=recall_score, cv=5)
-    clf2_score = clf2_cross.mean()
-    clf3 = SVC()
-    clf3_cross = cross_val_score(clf3, X_train, y_train, scoring=recall_score, cv=5)
-    clf3__score = clf3_cross.mean()
-    best_recall_val = max(clf1_score, clf2_score, clf3__score)
-    if best_recall_val == clf1_score:
-        best_clf = DecisionTreeClassifier()
-    elif best_recall_val == clf2_score:
-        best_clf = clf2
-    else:
-        best_clf = clf3
-    return best_clf, best_recall_val
--- Main Project.py
+++ Main Project.py
@@ -2,13 +2,16 @@ from CrawlingFunctions import create_soup_obj, all_hotels_parse, specific_hotel_
 from CleaningFunctions import remove_duplicative, remove_corrupt_rows, outlier_detection_zscore_dist
 from VisualizationFunctions import one_dim_plot, get_frequent_elements, two_dim_plot
 import matplotlib.pyplot as plt
-from MachineLearningFunctions import load_dataset, transfer_str_to_numeric_vals, split_to_train_and_test, \
-    find_best_model
+from MachineLearningFunctions import load_dataset, transfer_str_to_numeric_vals, split_to_train_and_test
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn import tree, metrics
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.naive_bayes import GaussianNB
 
 if __name__ == "__main__":
-    print("-- Start --")
+    # print("-- Start --")
     # num_of_hotel_pages = int(input("How many pages do you want to crawl? "))
-
+    #
     # tripadvisor_url_short = "https://www.tripadvisor.com"
     # tripadvisor_urls = ["https://www.tripadvisor.com/Hotels-g60763-New_York_City_New_York-Hotels.html",
     #                     "https://www.tripadvisor.com/Hotels-g187147-Paris_Ile_de_France-Hotels.html",
@@ -16,8 +19,7 @@ if __name__ == "__main__":
     #                     "https://www.tripadvisor.com/Hotels-g186338-London_England-Hotels.html",
     #                     "https://www.tripadvisor.com/Hotels-g274887-Budapest_Central_Hungary-Hotels.html",
     #                     "https://www.tripadvisor.com/Hotels-g28930-Florida-Hotels.html"]
-    # tripadvisor_urls = ["https://www.tripadvisor.com/Hotels-g60763-New_York_City_New_York-Hotels.html"]
-
+    #
     # print("-- Crawling --")
     # for url in tripadvisor_urls:
     #     soup = create_soup_obj(url)
@@ -44,17 +46,36 @@ if __name__ == "__main__":
     # plt.close()
 
     print("-- Machine Learning --")
-    file_name = "data frame after cleaning - old for test.csv"
+    file_name = "data frame after cleaning.csv"
     test_ratio, rand_state = 0.2, 42
     max_dep = 4
     min_smpl_splt = 5
-    category_col_name = 'class'
+    category_col_name = 'Tripadvisor rating'
     raw_dataset = load_dataset(file_name)
     dataset = transfer_str_to_numeric_vals(raw_dataset)
+    dataset.to_csv(r'data frame after cleaning - after change !.csv', index=False, header=True)
+    print(dataset.shape())
     X_train, X_test, y_train, y_test = split_to_train_and_test(dataset, category_col_name, test_ratio, rand_state)
     print(X_train.shape, X_test.shape, y_train.shape, y_test.shape)
-    best_clf, best_recall_val = find_best_model(X_train, y_train, max_dep, min_smpl_splt)
-    print(str(best_clf))
-    print(str(best_recall_val))
+    print(X_train)
+    print(X_test)
+    print(y_train)
+    print(y_test)
+    print("-------------------------------------------------")
 
-    print("--End--")
+    clf1 = tree.DecisionTreeClassifier()
+    clf2 = RandomForestClassifier()
+    clf3 = GaussianNB()
+    clf4 = KNeighborsClassifier()
+    algNames = ["decision_tree", "random_forest", "naive_bayes", "knn"]
+    for idx, clf in enumerate([clf1, clf2, clf3, clf4]):
+        clf.fit(X_train, y_train)
+        yPred = clf.predict(X_test)
+        yPredTrain = clf.predict(X_train)
+        print(algNames[idx])
+        print(f"accurate on train is {metrics.accuracy_score(y_true=y_train, y_pred=yPredTrain)}")
+        print(f"accurate on test is {metrics.accuracy_score(y_true=y_test, y_pred=yPred)}")
+        print("Confusion matrix is:")
+        print(metrics.confusion_matrix(y_test, yPred))
+        print("-------------------------------------------------")
+    print("--The End--")
